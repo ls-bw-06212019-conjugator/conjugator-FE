93f152d951668a5237ccad20d7a9194b43d85209
Clark Williams <theclarkwilliams@gmail.com>
2019-06-24 14:13:35 -0600

    navbar rendering in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       Test Text
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,13 @@ import './Navbar.scss';
 export const Navbar = props => {
   return (
     <div className='navbar'>
-      Navbar goes here
+      <h1>Conjugator</h1>
+      <nav>
+        <NavLink exact to="/">Practice</NavLink>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+        <NavLink to="/settings">Settings</NavLink>
+        <NavLink to="/auth">Login/Sign Up</NavLink>
+      </nav>
     </div>
   )
 }
