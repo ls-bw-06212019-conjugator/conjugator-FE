2ca097d57abf32d8e962234cdd41544f6ff54121
Clark Williams <theclarkwilliams@gmail.com>
2019-06-25 22:32:12 -0600

    made english font slightly smaller

--- src/components/Conjugator/Conjugator.js
+++ src/components/Conjugator/Conjugator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Alert, Button, Collapse, Spinner } from "reactstrap";
+import { Button, Collapse, Spinner } from "reactstrap";
 import { desktopHelp } from "../../img/desktop-accent-instructions.jpg";
 import { mobileHelp } from "../../img/mobile-accent-instructions.png";
 import { getWord } from '../../actions';
@@ -37,11 +37,7 @@ export const Conjugator = connect(
     }
 
     componentWillMount() {
-      
       this.props.getWord();
-      // this.setState({
-      //   wordInput: ""
-      // })
     }
     componentDidMount() {
       this.updatePredicate();
@@ -164,7 +160,6 @@ export const Conjugator = connect(
           </form>
 
           <Button color="link" className="skip small-bot-marg" onClick={this.skipWord}>Skip this Word</Button>
-          {/* <Alert color="danger" className={this.state.isWrong ? "alert" : "alert hidden"}>Incorrect Answer!</Alert> */}
           <Button color="danger" className={this.state.isWrong ? "small-bot-marg" : "small-bot-marg hidden"} onClick={this.toggleCollapse}>
             Incorrect Answer! Click to Show Answer
           </Button>          
